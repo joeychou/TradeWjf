ca459d8e321d511ccba73e88979c874ca33270c5
506179512@qq.com <506179512@qq.com>
2020-06-19 21:29:16 +0800

    2130

--- BLL/tasks_k_t_line.cs
+++ BLL/tasks_k_t_line.cs
@@ -168,9 +168,9 @@ namespace BLL
                                 {
                                     rd_txt += "10派" + (m_fProfit) + "元(含税)";
                                 }
-                                stringLot.Add("update sys_stock_code set rd_deal='" + Utils.StrToDateTime(rd_deal.ToString()).ToString("yyyy-MM-dd HH:mm:ss") + "',rd_num="
+                                stringLot.Add("update sys_stock_code set remark='" + rd_deal + "',rd_num="
                                 + (m_fGive / 10) + ",rd_money=" + (m_fProfit / 10) + ",rd_txt='" + rd_txt + "',input_time=now() where stock_code='" + stock_code + "';");
-
+                                stringLot.Add($"update sys_stock_code set rd_deal=remark where stock_code='{stock_code}';");
                                 //Log.WriteLog("除权降息，手动操作", $"sc={sc},stock_code={stock_code},RQ={rd_deal},xxlx={xxlx},m_fProfit={m_fProfit},m_fGive={m_fGive},F3={F3},F4={F4},i={i},z={z},lgt={lgt}");
                             }
                         }
